147cbef898b2775b48312010d562553d11dcc934
Grady Robbins <gradyrobbins13@gmail.com>
2019-11-10 23:34:34 -0600

    added main.js and experimenting with a function for :hover on wireframe

---
+++ Class1/main.js
@@ -0,0 +1,8 @@
+console.log("hello main.js")
+
+function chbg(element, color, size) {
+    document.getElementById(element).style.backgroundColor = color;
+    document.getElementById(element).style.backgroundColor = color;
+    document.getElementById(element).style.fontSize = size;
+}
+
