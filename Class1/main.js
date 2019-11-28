bb23b61e4590e5fb092c430b4c264ccc32fa62f0
Grady Robbins <gradyrobbins13@gmail.com>
2019-11-27 23:30:46 -0600

    better formatting

--- Class1/main.js
+++ Class1/main.js
@@ -2,28 +2,65 @@ console.log("hello main.js")
 
 function chbg(color, size, font) {
     document.getElementById("a").style.backgroundColor = color;
-    document.getElementById("b").style.fontFamily = font;
     document.getElementById("b").style.backgroundColor = color;
     document.getElementById("b").style.fontSize = size;
+    document.getElementById("b").style.fontFamily = font;
+
 }
 
-function chbg2(color, size) {
+function chbg2(color, size, font) {
     document.getElementById("c").style.backgroundColor = color;
     document.getElementById("d").style.backgroundColor = color;
     document.getElementById("d").style.fontSize = size;
-}
-function chbg3(color, size) {
+    document.getElementById("d").style.fontFamily = font;
     document.getElementById("e").style.backgroundColor = color;
+    document.getElementById("e").style.fontSize = size;
+    document.getElementById("e").style.fontFamily = font;
     document.getElementById("f").style.backgroundColor = color;
     document.getElementById("f").style.fontSize = size;
+    document.getElementById("f").style.fontFamily = font;
+
 }
-function chbg4(color, size) {
+
+function chbg3(color, size, font) {
     document.getElementById("g").style.backgroundColor = color;
     document.getElementById("h").style.backgroundColor = color;
     document.getElementById("h").style.fontSize = size;
-}
-function chbg5(color, size) {
+    document.getElementById("h").style.fontFamily = font;
     document.getElementById("i").style.backgroundColor = color;
+    document.getElementById("i").style.fontSize = size;
+    document.getElementById("i").style.fontFamily = font;
     document.getElementById("j").style.backgroundColor = color;
     document.getElementById("j").style.fontSize = size;
+    document.getElementById("j").style.fontFamily = font;
+
 }
+
+function chbg4(color, size, font) {
+    document.getElementById("k").style.backgroundColor = color;
+    document.getElementById("l").style.backgroundColor = color;
+    document.getElementById("l").style.fontSize = size;
+    document.getElementById("l").style.fontFamily = font;
+    document.getElementById("m").style.backgroundColor = color;
+    document.getElementById("m").style.fontSize = size;
+    document.getElementById("m").style.fontFamily = font;
+    document.getElementById("n").style.backgroundColor = color;
+    document.getElementById("n").style.fontSize = size;
+    document.getElementById("n").style.fontFamily = font;
+
+}
+
+function chbg5(color, size, font) {
+    document.getElementById("o").style.backgroundColor = color;
+    document.getElementById("p").style.backgroundColor = color;
+    document.getElementById("p").style.fontSize = size;
+    document.getElementById("p").style.fontFamily = font;
+    // document.getElementById("m").style.backgroundColor = color;
+    // document.getElementById("m").style.fontSize = size;
+    // document.getElementById("m").style.fontFamily = font;
+    // document.getElementById("n").style.backgroundColor = color;
+    // document.getElementById("n").style.fontSize = size;
+    // document.getElementById("n").style.fontFamily = font;
+
+}
+
